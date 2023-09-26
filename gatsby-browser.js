aa03a09dde06378746e4edf003dfc5e27167985d
Anastasia Kashkinova <anastasia.kashkinova@gmail.com>
2023-09-26 14:45:19 +1000

    bring back modal

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,11 @@
  */
 
 // You can delete this file if you're not using it
+
+export const onRouteUpdate = () => {
+  const modalScripts = document.createElement("script")
+  modalScripts.id = "modalScripts"
+  modalScripts.src = "/scripts/modal.js"
+  modalScripts.async = true
+  document.body.appendChild(modalScripts)
+}
